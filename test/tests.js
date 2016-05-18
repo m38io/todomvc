81811e52bfee7f5e9ed9d73f0f43754d5278d922
Evgeny Rahman <erahman@massmutual.com>
2016-05-18 10:43:04 -0400

    Adding testing with Mocha

---
+++ test/tests.js
@@ -0,0 +1,23 @@
+var request = require("request"),
+    assert = require('assert'),
+    expect = require('expect'),
+    server = require("../app.js"),
+    base_url = "http://localhost:8080/";
+
+describe("TodoMVC Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns TodoMVC page", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toInclude("<title>React • TodoMVC</title>");
+        done();
+      });
+    });
+  });
+});
