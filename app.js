57a07d897ab6d0c60d4c7de6c48f38f36b21e13f
Evgeny Rahman <erahman@massmutual.com>
2016-05-18 12:50:51 -0400

    Changing to use restify and moving the static site to a public directory

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-var http = require("http");
-var fs = require("fs");
+var fs = require('fs');
+var path = require('path');
+var getopt = require('posix-getopt');
+var restify = require('restify');
 
-http.createServer(function (request, response) {
+(function main() {
 
-  fs.readFile('index.html', function(err, data) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    // Create Server
+    var server = restify.createServer({
+        name: 'todomvc',
+        version: '1.0.0'
+    });
 
-    response.end(data);
-  });
-}).listen(8080);
+    // Ensure only JSON formatted requests are sent through
+    server.pre(function (req, res, next) {
+
+        if (req.url == '/') {
+            req.url = '/index.html'
+        }
+        return next();
+    });
+
+    // Configure server listening port
+    server.listen(8080, function () {
+        console.log('%s listening at %s', server.name, server.url);
+    });
+
+    // Parses out the Accept header, and ensures that the server can respond to what the client asked for
+    server.use(restify.acceptParser(server.acceptable));
+
+    // Parses out the HTTP Date header (if present) and checks for clock skew
+    server.use(restify.dateParser());
+
+    // Parses the HTTP query string (i.e., /foo?id=bar&name=mark
+    server.use(restify.queryParser({mapParams: true}));
+
+    // Configure body parse to allow reading the payload
+    server.use(restify.bodyParser({mapParams: true}));
+
+    // Enable CORS support
+    server.pre(restify.CORS({
+        origins: ['*']
+    }));
+
+    // Serve static page
+    server.get(/\/*/, restify.serveStatic({
+        directory: './public',
+    }));
+})();
