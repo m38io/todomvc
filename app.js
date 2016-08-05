d6a64c8c10fddbecb95dc1feeb310ec3a99fd355
Evgeny Rahman <evgeny.rahman@gmail.com>
2016-08-05 11:17:17 -0400

    Changing address to 0.0.0.0
    0.0.0.0 works better than localhost for running inside Vagrant and then inside Docker.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var restify = require('restify');
     });
 
     // Configure server listening port
-    server.listen(8080, 'localhost', function () {
+    server.listen(8080, '0.0.0.0', function () {
         console.log('%s listening at %s', server.name, server.url);
     });
 
